492c45468627d7469ff45a50526ed9cb387afd47
exonkola <91274647+exonkola@users.noreply.github.com>
2023-01-09 10:35:41 +0000

    first

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,45 @@
-import React, { useState } from 'react';
-import reactLogo from './assets/react.svg';
+import React from 'react';
 import './App.css';
 
+const course = [
+  { nom : ' fromage ',},
+  { nom : ' farine' , },
+];
+
+
+
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((countParam) => countParam + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </div>
+   <div>
+
+          <h1>liste des courses</h1>
+
+
+          <Course />
+   </div>
+
+
+
   );
 }
+
+ function Course (){
+  return (
+
+   <ul>
+       {course.map(function displayTerm(term) {
+         return (
+           <li key={term.objectID}>
+             <span>
+               {term.nom}
+             </span>
+           </li>
+         );
+       })}
+   </ul>
+   );
+ }
+
 
 export default App;
